7b939aabe154291e1bb31719e95ec1a9b609ba4c
Petersen-Andrew <Petersen-Andrew@users.noreply.github.com>
2018-06-21 14:45:28 -0700

    Week 9 Read Me First Stuff

---
+++ cityOfAaron/src/view/ViewInterface.java
@@ -0,0 +1,17 @@
+/*
+ * The View Interface
+ * Date last modified: June 2018
+ */
+package view;
+
+/**
+ *
+ * @author andyp
+ */
+public interface ViewInterface {
+    
+    public void displayMenu();
+    public int getMenuOption();
+    public void doAction(int option);
+    
+}
